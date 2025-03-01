d9929a2e0e11f59f3d94f2a5ce7c10199c7c3ea6
Eric Wuehler <ewuehler@gmail.com>
2025-02-28 21:42:22 -0800

    updated

--- bsky.sh
+++ bsky.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-fname="$HOME/mostlysecurity/finals/mostlysecurity${1}.mp3"
+
+if echo "$HOME" | grep -q "wuehler"; then
+    echo "Eric's System"
+    fname="$HOME/Podcast/mostlysecurity${1}.mp3"
+else
+    echo "Jon's System"
+    fname="$HOME/mostlysecurity/finals/mostlysecurity${1}.mp3"
+fi
+
 
 #./posttobsky.py -i ~/Podcast/mostlysecurity377.mp3 
 echo -e "./posttobsky.py -i ${fname}"
